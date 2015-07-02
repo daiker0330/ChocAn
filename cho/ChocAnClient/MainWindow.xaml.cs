6337be679bea2e88118272f81aae299c492d0e55
yjwRank <15901268179@163.com>
2015-07-02 14:55:17 +0800

    加了界面
    要修改的话要把主窗口的那个xaml文件的第6-24行 和 28-48行 注释掉才行

--- cho/ChocAnClient/MainWindow.xaml.cs
+++ cho/ChocAnClient/MainWindow.xaml.cs
@@ -24,9 +24,15 @@ namespace ChocAnClient
     {
         string result;
         RemoteProcessCall network = new RemoteProcessCall();
+        /*****************UI*******************************/
+        public bool full_small = true;
+        public double w_left = 0;
+        public double w_top = 0;
+        /*****************UI******************************/
         public MainWindow()
         {
             InitializeComponent();
+            InitWindowLocalInCenter();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -81,5 +87,91 @@ namespace ChocAnClient
             double recv = network.GetProviderSum("123456789");
             ((Button)sender).Content = "GetProviderSum(" + recv.ToString() + ")";
         }
+
+        /**************************以下是UI的代码******************************************************************/
+        public void InitWindowLocalInCenter()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            caozuo.Visibility = System.Windows.Visibility.Visible;
+            deng.Visibility = System.Windows.Visibility.Hidden;
+            JiZhang.Visibility = System.Windows.Visibility.Hidden;
+        }
+        private void SignIn_Button(object sender, RoutedEventArgs e)
+        {
+            caozuo.Visibility = System.Windows.Visibility.Hidden;
+            deng.Visibility = System.Windows.Visibility.Visible;
+            JiZhang.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void JiZhang_Button(object sender, RoutedEventArgs e)
+        {
+            caozuo.Visibility = System.Windows.Visibility.Hidden;
+            deng.Visibility = System.Windows.Visibility.Hidden;
+            JiZhang.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void BackHome_Button(object sender, RoutedEventArgs e)
+        {
+            caozuo.Visibility = System.Windows.Visibility.Visible;
+            deng.Visibility = System.Windows.Visibility.Hidden;
+            JiZhang.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            caozuo.Visibility = System.Windows.Visibility.Visible;
+            deng.Visibility = System.Windows.Visibility.Hidden;
+            JiZhang.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void fullScreen_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (full_small)
+            {
+                w1.Height = SystemParameters.MaximizedPrimaryScreenHeight - 8d;
+                w1.Width = SystemParameters.MaximizedPrimaryScreenWidth - 8d;
+                WindowStartupLocation = WindowStartupLocation.Manual;
+                w1.Left = 0; w1.Top = 0;
+
+            }
+            else
+            {
+                w1.Height = 350;
+                w1.Width = 525;
+                w1.Top = (SystemParameters.MaximizedPrimaryScreenHeight - 8d) / 2 - Height / 2;
+                w1.Left = (SystemParameters.MaximizedPrimaryScreenWidth - 8d) / 2 - Width / 2;
+                WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+            full_small = !full_small;
+        }
+
+        private void MuLu_Click(object sender, RoutedEventArgs e)
+        {
+            SuplierList sup = new SuplierList();
+            sup.Owner = this;
+            sup.ShowDialog();
+        }
+
+        private void Button_Click_Submit(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("提交到服务器");
+        }
+
+        private void TiJiao_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("存盘");
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("给服务器发消息发邮件");
+
+        }
+        /**************************UI的代码******************************************************************/
     }
 }
