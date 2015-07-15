9b4491854a987c8306181f94c9bc0bcc4ca0a7dd
DarkForte <laserae86@126.com>
2015-07-15 15:11:31 +0800

    增加负载测试

---
+++ cho/NetworkInterfaceUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetworkInterfaceUnitTest
+{
+    [TestClass]
+    public class SingleTest : TestBase
+    {
+        [TestMethod]
+        public void InitTest()
+        {
+            network.init();
+        }
+    }
+}
